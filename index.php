22a49cf2aa431e5642e5aee42a9c48807bc8f06c
LucasP <luckaas.pereira@gmail.com>
2016-08-31 22:41:42 -0400

    Model Model / Index 2.3

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $controller = $separador[0];
 $action = $separador[1] == NULL ? "index" : $separador[1];
 
 require_once "system/controller.php";
+require_once "system/model.php";
 require_once "application/controllers/" . $controller . ".php";
 
 $app = new $controller();
---
+++ system/model.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of model
+ *
+ * @author Lucaas
+ */
+class Model {
+    
+}
