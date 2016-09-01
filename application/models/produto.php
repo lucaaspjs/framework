62762cbe0e22959a45cf540a86c84a178dfe76fd
LucasP <luckaas.pereira@gmail.com>
2016-08-31 23:11:30 -0400

    Model Pai 2.0 / model produto

---
+++ application/models/produto.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of produtos
+ *
+ * @author Lucaas
+ */
+class Produtos extends Model {
+
+    protected $tabela = "produtos";
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
--- system/model.php
+++ system/model.php
@@ -11,13 +11,39 @@ class Model {
     private $db_name = "framework";
     private $db_user = "root";
     private $db_pass = "root";
+
+    /**
+     * @var PDO 
+     */
     private $conn;
+    public $id; #id do objeto
+    public $data; #dados do objeto
+    protected $tabela; #tabela relacionada ao objeto
 
     public function __construct() {
-        $dns = "mysql:host" . $this->db_host . ";dbname=" . $this->db_name;
-        $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+        $dsn = "mysql:host" . $this->db_host . ";dbname=" . $this->db_name;
+        $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 
         $this->conn = new PDO($dsn, $this->db_user, $this->db_pass, $options);
+
+        $this->setAttributes();
+    }
+
+    private function setAttributes() {
+        $sql = "DESCRIBE {$this->tabela}";
+        $q = $this->conn->prepare($sql);
+        $q->execute();
+
+        $attributes = $q->fetchAll(PDO::FETCH_COLUMN);
+
+        foreach ($attributes as $field) {
+            if ($field == id) {
+                continue;
+            }
+
+            $this->data[$field] = NULL;
+            $this->field = &$this->data[$field];
+        }
     }
 
 }
