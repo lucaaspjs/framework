3132bd97fcb965c8a153b2a35bcea5e2e253f17d
LucasP <luckaas.pereira@gmail.com>
2016-08-31 22:50:20 -0400

    Model Pai 1.0

--- system/model.php
+++ system/model.php
@@ -6,5 +6,18 @@
  * @author Lucaas
  */
 class Model {
-    
+
+    private $db_host = "localhost";
+    private $db_name = "framework";
+    private $db_user = "root";
+    private $db_pass = "root";
+    private $conn;
+
+    public function __construct() {
+        $dns = "mysql:host" . $this->db_host . ";dbname=" . $this->db_name;
+        $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+        $this->conn = new PDO($dsn, $this->db_user, $this->db_pass, $options);
+    }
+
 }
