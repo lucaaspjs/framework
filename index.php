b07b2aa018001c31a04b98801536ab0b17f9a726
LucasP <luckaas.pereira@gmail.com>
2016-08-31 22:25:13 -0400

    Index 2.0

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-$key = isset($_GET['key']) ? $_GET['key'] : "app/index";
+$key = isset($_GET['key']) ? $_GET['key'] . "/" : "app/index";
 
 $separador = explode("/", $key);
 $controller = $separador[0];
-$action = $separador[1];
+$action = $separador[1] == NULL ? "index" : $separador[1];
+
+
+require_once "application/controllers/" . $controller . ".php";
+
+$app = new $controller();
+$app->$action();
